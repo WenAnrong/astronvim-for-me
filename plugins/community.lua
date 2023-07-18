c813e1adfdb393a103332017d0d0b3709dc35d00
WenAnrong <warhavefun@163.com>
2023-07-18 15:33:11 +0800

    first commit

---
+++ plugins/community.lua
@@ -0,0 +1,16 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
+}
